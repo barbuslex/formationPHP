82d28ef914136a4bf7f3fe7a2a55700a7e57d899
Barbuslex <barbuslex@gmail.com>
2025-03-18 00:03:34 +0100

    ajout exercice 4

---
+++ exercice4/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 2;
+$b = 4;
+
+$addition = $a + $b;
+$soustraction = $a - $b;
+$multiplication = $a * $b;
+$division = $a / $b;
+$divisionEntiere = $a % $b;
+
+echo "Calculatrice:\n";
+echo "A = $a\n";
+echo "B = $b\n";
+echo "Addition (a+b) = $addition\n";
+echo "Soustraction (a-b) = $soustraction\n";
+echo "Multiplication (a*b) = $multiplication\n";
+echo "Division (a/b) = $division\n";
+echo "Division entière (a%b) = $divisionEntiere\n";
