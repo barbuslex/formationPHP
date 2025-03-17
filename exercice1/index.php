1030dceab2bbbf53131f1e7633f581bc9076b8df
Barbuslex <barbuslex@gmail.com>
2025-03-18 00:03:04 +0100

    ajout exercice 1

---
+++ exercice1/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$prenom = "Mathis";
+$age = 20;
+$isEtudiant = true;
+
+echo $prenom;
+echo '\n';
+echo "\n";
+
+echo $age."\n";
+echo $isEtudiant."\n";
+
+print_r($isEtudiant);
+
+echo '<pre>';
+var_dump($isEtudiant);
+echo '</pre>';
+
+var_dump($isEtudiant);
+var_dump($prenom, $age, $isEtudiant);
+
