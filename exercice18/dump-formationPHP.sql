7efd4163b8455ea4007d49b6ea3a3ba5dac3296e
Barbuslex <barbuslex@gmail.com>
2025-03-26 15:34:16 +0100

    fix exercice 18

--- exercice18/dump-formationPHP.sql
+++ exercice18/dump-formationPHP.sql
@@ -23,8 +23,10 @@ DROP TABLE IF EXISTS `connexions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `connexions` (
-  `connexion_date` datetime DEFAULT NULL,
-  `connexion_utilisateur` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL
+  `id` int NOT NULL AUTO_INCREMENT,
+  `connexion_date` datetime NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -45,7 +47,9 @@ DROP TABLE IF EXISTS `utilisateurs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `utilisateurs` (
-  `utilisateur_nom` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -55,7 +59,6 @@ CREATE TABLE `utilisateurs` (
 
 LOCK TABLES `utilisateurs` WRITE;
 /*!40000 ALTER TABLE `utilisateurs` DISABLE KEYS */;
-INSERT INTO `utilisateurs` VALUES ('bob');
 /*!40000 ALTER TABLE `utilisateurs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -72,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-26 15:22:49
+-- Dump completed on 2025-03-26 15:33:34
