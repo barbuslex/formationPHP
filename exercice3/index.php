39f8cfd925ae4a7fd7cccba9b6314737c7a4c2c5
Barbuslex <barbuslex@gmail.com>
2025-03-26 12:13:59 +0100

    mise à jour exercice 3

--- exercice3/index.php
+++ exercice3/index.php
@@ -1,5 +1,30 @@
 <?php 
-class Personne
+echo "Personne BAD Method : \n";
+class PersonneBad
+{
+    var string $prenom;
+    var int $age;
+    var bool $isEtudiant;
+
+    public function __construct(string $prenom, int $age, bool $isEtudiant)
+    {
+        $this->prenom = $prenom;
+        $this->age = $age;
+        $this->isEtudiant = $isEtudiant;
+    }
+}
+
+$Alex = new PersonneBad('Alex', 15, true);
+$John = new PersonneBad('John', 18, true);
+$Mike = new PersonneBad('Mike', 32, false);
+
+echo $Alex->prenom." a ".$Alex->age." ans\n";
+echo $John->prenom." a ".$John->age." ans\n";
+echo $Mike->prenom." a ".$Mike->age." ans\n";
+echo "\n";
+
+echo "Personne GOOD Method :\n";
+class PersonneGood
 {
     private string $prenom;
     private int $age;
@@ -52,9 +77,9 @@ class Personne
     }
 }
 
-$Alex = new Personne('Alex', 15, true);
-$John = new Personne('John', 18, true);
-$Mike = new Personne('Mike', 32, false);
+$Alex = new PersonneGood('Alex', 15, true);
+$John = new PersonneGood('John', 18, true);
+$Mike = new PersonneGood('Mike', 32, false);
 
 echo $Alex->getPrenom()." a ".$Alex->getAge()." ans\n";
 echo $John->getPrenom()." a ".$John->getAge()." ans\n";
